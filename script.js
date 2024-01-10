33137a1f26fef895d9d38388f49b11df9d2832b7
David Excel <davidsome2004@gmail.com>
2024-01-10 16:35:35 +0100

    My First commit

---
+++ script.js
@@ -0,0 +1,84 @@
+// var cart = ['Fish', 'Meat', 'Snail', 'Egg', 'Plantain', 'Beef'];
+// console.log(cart[5]);//
+
+// cart.pop();
+// cart.shift();
+// cart.push('cammel meat');
+// cart.unshift('goat meat');
+// console.log(cart);
+// disp.innerHTML = cart;
+
+var purchase = [];
+console.log(purchase);
+
+function displayPurchase() {
+    show.innerHTML = "";
+    for (i = 0; i < purchase.length; i++) {
+        show.innerHTML += `<p>${i + 1}. ${purchase[i]}</p>`;
+    }
+}
+
+function addItems() {
+    if (inp.value !== '') {
+        console.log(inp.value);
+        purchase.push(inp.value);
+        document.getElementById('inp').value = '';
+        console.log(purchase);
+        displayPurchase()
+    } else {
+        alert("Fill in something");
+        console.error("It's Empty");
+    }
+}
+
+function addFirst() {
+    if (inp.value !== '') {
+        console.log(inp.value);
+        purchase.unshift(inp.value);
+        document.getElementById('inp').value = '';
+        console.log(purchase);
+        show.innerHTML = "";
+        displayPurchase()
+    } else {
+        alert("Fill in something");
+        console.error("It's Empty");
+    }
+}
+
+function deleteFirst() {
+    if (inp.value === '') {
+        purchase.splice(0, 1)
+        console.log(purchase);
+        displayPurchase()
+    } else {
+        alert("Fill in something");
+        console.error("It's Empty");
+    }
+}
+
+function deleteLast() {
+    if (inp.value === '') {
+        purchase.pop(inp.value)
+        console.log(purchase);
+        displayPurchase()
+    } else {
+        alert("Fill in something");
+        console.error("It's Empty");
+    }
+}
+
+function deleteAny(){
+
+}
+
+
+function deleteAll() {
+    if (inp.value === '') {
+        purchase.splice(0, purchase.length)
+        console.log(purchase);
+        displayPurchase()
+    } else {
+        alert("Fill in something");
+        console.error("It's Empty");
+    }
+}
